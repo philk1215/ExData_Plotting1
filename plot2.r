c43c400326a40f57811e17cf99edfddb0f216aaa
Philip Kreckel <philk1215@gmail.com>
2014-07-13 17:13:29 -0400

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,5 +1,5 @@
 x <- read.table("PwrData.txt", header=TRUE, sep=";")
 png('plot2.png')
-plot(x$Global_active_power,xlab="Date",ylab="Power",main="Global Active Power",type="n")
+plot(x$Global_active_power,xlab="Date",ylab="Power",type="n")
 lines(x$Global_active_power, main = "Global Active Power")
 dev.off()
